d7d0c2305317e5a610f542324ee270be2bf74e48
StanleyCocos <stanleycocoa@gmail.com>
2021-11-23 15:47:59 +0800

    添加iOS打开设置

--- ios/Classes/SwiftFlutterOpenSettingPlugin.swift
+++ ios/Classes/SwiftFlutterOpenSettingPlugin.swift
@@ -10,5 +10,27 @@ public class SwiftFlutterOpenSettingPlugin: NSObject, FlutterPlugin {
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     result("iOS " + UIDevice.current.systemVersion)
+    
+    if "openSetting" == call.method {
+        guard let url = URL.init(string: UIApplication.openSettingsURLString) else {
+            result(false)
+            return;
+        }
+        if UIApplication.shared.canOpenURL(url){
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(url)
+            } else {
+                UIApplication.shared.openURL(url)
+            }
+        } else {
+            result(false)
+        }
+        result(true)
+    } else {
+        result("iOS " + UIDevice.current.systemVersion)
+    }
+    
+
   }
 }
+
